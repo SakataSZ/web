0ecb2d583959f8eb3d68d7af9aef03f43373eaf7
SakataSZ <sakamoto.yuta.sz@gmail.com>
2023-12-23 22:38:20 +0900

    feat: add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,6 +9,12 @@ export default defineConfig({
   },
   integrations: [tailwind()],
   markdown: {
-    shikiConfig: { theme: "rose-pine" },
+    shikiConfig: {
+      theme: "rose-pine",
+    },
   },
+  output: "server",
+  adapter: vercel({
+    webAnalytics: true,
+  }),
 });
